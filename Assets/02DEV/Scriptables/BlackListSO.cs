eaf62108540331ad098ff6f1e377f318cc506778
HakkiSahin <34152747+HakkiSahin@users.noreply.github.com>
2025-03-08 09:08:54 +0300

    blocked world controller

---
+++ Assets/02DEV/Scriptables/BlackListSO.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BlackListSO", menuName = "Scriptable Objects/BlackListSO")]
+public class BlockListSO : ScriptableObject
+{
+
+    public List<BlockedList> blockedLists;
+
+    [System.Serializable]
+    public struct BlockedList
+    {
+        public string dayIndex;
+        public List<string> blockedNameWord;
+        public List<string> blockedDescriptionWord;
+    }
+}
